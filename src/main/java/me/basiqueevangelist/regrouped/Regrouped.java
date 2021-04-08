72a0620582ff06189d48ac58eb06661d07c6be7e
Basique Evangelist <basiqueevangelist@yandex.ru>
2021-04-08 22:58:41 +0300

    Basic mod skeleton

---
+++ src/main/java/me/basiqueevangelist/regrouped/Regrouped.java
@@ -0,0 +1,10 @@
+package me.basiqueevangelist.regrouped;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Regrouped implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
